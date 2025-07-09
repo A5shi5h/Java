45d707d6985373b3a0d0283ac0baa4bf1302c0fd
Ashish Lamichaney <124287828+A5shi5h@users.noreply.github.com>
2025-07-09 16:15:34 +0530

    reverse a string

--- helloWorld/src/ReverseString.java
+++ helloWorld/src/ReverseString.java
@@ -3,14 +3,14 @@ package helloWorld.src;
 import java.util.Scanner;
 
 public class ReverseString {
-    public static void main(String args[]){
+    public static void main(String[] args){
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a String");
         String str = sc.next();
-        String str2 = "";
+        StringBuilder str2 = new StringBuilder();
         for( int i = str.length()-1 ; i >= 0 ; i--){
-           str2 += str.charAt(i);
+           str2.append(str.charAt(i));
         }
         System.out.println(str2);
     }
